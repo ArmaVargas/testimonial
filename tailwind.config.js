26b9a6dffb15906e2f873e8f8b71fa036bb17c69
ArmaVargas <armando091994@gmail.com>
2024-09-26 15:12:39 -0500

    termiando el html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ModerateViolet": "hsl(263, 55%, 52%)",
+        "VeryDarkGrayishBlue": "hsl(217, 19%, 35%)",
+        "VeryDarkBlackishBlue": "hsl(219, 29%, 14%)",
+        "White": "hsl(0, 0%, 100%)",
+        "LightGray": "hsl(0, 0%, 81%)",
+        "LightGrayishBlue": "hsl(210, 46%, 95%)",
+      },
+
+      fontFamily:{
+        'Barlow':["Barlow Semi Condensed", "sans-serif"]
+      },
+      backgroundImage:{
+        'comilla': "url('./images/bg-pattern-quotation.svg')"
+      },
+    },
   },
   plugins: [],
-}
+};
